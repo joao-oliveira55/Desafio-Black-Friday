e500c2f1508027584cd2b3585ae76c71b5cf6211
joao-oliveira55 <ash.joao55@gmail.com>
2021-07-15 17:27:34 -0300

    Feature: adicionando validação de email

--- src/components/formBlackFriday/index.js
+++ src/components/formBlackFriday/index.js
@@ -1,11 +1,28 @@
 import React, {useState} from "react";
 import * as S from './styled'
+const { v4: uuidv4 } = require('uuid');
 
 export default function Header() {
     const [ email, setEmail ] = useState('')
+    const [ messageForm, setMessageForm ] = useState('')
+
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
 
     function handleEmail (){
-        localStorage.setItem('emailBlackFriday', email)
+        if (validateEmail(email) === true){
+
+            localStorage.setItem(uuidv4(), email)
+            setMessageForm('Email successfully sent!!!')
+            setEmail('')
+
+        }else{
+
+            setMessageForm('Invalid email, try again.')
+
+        }
     }
     
     return (
@@ -16,6 +33,7 @@ export default function Header() {
                     <input type="email" value={email} required placeholder="Email" onChange={e => setEmail(e.target.value)} />
                     <button onClick={handleEmail}>Submit</button>
                 </div>
+                <span>{messageForm}</span>
             </div>
         </S.sectionMiddle>
     );
